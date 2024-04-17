0f46016b2b5ff96d2f9fc57b7dad0c97d504500b
Nouman Baloch <nomibaloch775@hotmail.com>
2024-04-18 04:34:47 +0500

    Add project files.

---
+++ UserManagement.Infrastructure/Contexts/UserManagementDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Infrastructure.Contexts;
+public class UserManagementDbContext : DbContext
+{
+    public UserManagementDbContext(DbContextOptions<UserManagementDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    => optionsBuilder
+        .UseSqlServer()
+        .UseSnakeCaseNamingConvention();
+
+    public DbSet<UserEntity> AppUsers => Set<UserEntity>();
+}
